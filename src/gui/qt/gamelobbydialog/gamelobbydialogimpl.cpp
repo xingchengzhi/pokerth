4e6db83e06d14564a4916c1439fddfe33bb4042a
q4z1 <kphilipp@inquies.de>
2026-01-10 17:13:06 +0100

    macOS: fix missing titlebar for lobby view

--- src/gui/qt/gamelobbydialog/gamelobbydialogimpl.cpp
+++ src/gui/qt/gamelobbydialog/gamelobbydialogimpl.cpp
@@ -49,10 +49,7 @@ gameLobbyDialogImpl::gameLobbyDialogImpl(startWindowImpl *parent, ConfigFile *c)
 	: QDialog(parent), myW(NULL), myStartWindow(parent), myConfig(c), currentGameName(""), myPlayerId(0), isGameAdministrator(false), inGame(false), guestMode(false), blinkingButtonAnimationState(true), myChat(NULL), keyUpCounter(0), infoMsgToShowId(0), currentInvitationGameId(0), inviteDialogIsCurrentlyShown(false), autoStartTimerCounter(0), lastNickListFilterState(0)
 {
 
-#ifdef __APPLE__
-	// Don't use ApplicationModal on macOS - it disables the parent's menu
-	setWindowFlags(Qt::WindowSystemMenuHint | Qt::CustomizeWindowHint | Qt::Dialog);
-#elif _WIN32
+#ifdef _WIN32
 //	setWindowFlags(Qt::Dialog | Qt::WindowMinimizeButtonHint);
 #endif
 	setupUi(this);
